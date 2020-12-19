ac5eeeb7aa54e6e89a5b0ba0c39e2b84f32d6298
Jaja Brown <65190496+jbrown827@users.noreply.github.com>
2020-12-18 23:14:30 -0600

    Declaring function

--- script.js
+++ script.js
@@ -5,18 +5,31 @@
 var lower = "abcdefghijklmnopqrstuvwxyz";
 var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var nums = "0123456789";
-var spec = "~!@#$%/^+&*-()[]_{}<>?";
+var spec = "~!@#$%^+&*-()[/]_{:}<=>?";
 
 // User input variables
 
 var userInput = prompt(
-  "Please choose a number from 8 to 128 for your password length"
+  "Please choose a number between 8 and 128 for your password length"
 );
 
 var alphaLow = confirm("Would you like to use lowercase letters?");
 var alphaCap = confirm("Would you like to use uppercase letters?");
 var numbers = confirm("Would you like to you use numbers also?");
 var charSpec = confirm("How about special characters?");
+
+/*    -----------------------------------   */
+
+/* Function Declaration */
+
+function gerneratePassword() {
+  if (userInput >= 8 && userInput <= 128) {
+    for (var i = 0; i < userInput; i++) {
+      if (alphaLow && alphaCap && numbers && charSpec) {
+      }
+    }
+  }
+}
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");

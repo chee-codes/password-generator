40acf7af674018c7eb9a491093c61ac6c5fdcbf4
Jaja Brown <65190496+jbrown827@users.noreply.github.com>
2020-12-19 01:13:02 -0600

    the longest code ever

--- script.js
+++ script.js
@@ -26,15 +26,103 @@ var charSpec = confirm("How about special characters?");
 
 /* Function Declaration */
 
-function gerneratePassword() {
+function generatePassword() {
   if (userInput >= 8 && userInput <= 128) {
     for (var i = 0; i < userInput; i++) {
       if (alphaLow && alphaCap && numbers && charSpec) {
         pWord += allChar.charAt(Math.floor(Math.random() * allChar.length));
+      } else if (
+        alphaLow === true &&
+        alphaCap === true &&
+        numbers === true &&
+        charSpec === false
+      ) {
+        pWord += charOp1.charAt(Math.floor(Math.random() * charOp1.length));
+
+        //lower + upper + Special
+      } else if (
+        alphaLow === true &&
+        alphaCap === true &&
+        charSpec === true &&
+        numbers === false
+      ) {
+        pWord += charOp2.charAt(Math.floor(Math.random() * charOp2.length));
+
+        //lower + Numbers + Special
+      } else if (
+        alphaLow === true &&
+        numbers === true &&
+        charSpec === true &&
+        alphaCap === false
+      ) {
+        pWord += charOp3.charAt(Math.floor(Math.random() * charOp3.length));
+
+        // Upper + Numbers + Special
+      } else if (
+        alphaCap === true &&
+        numbers === true &&
+        charSpec === true &&
+        alphaLow === false
+      ) {
+        pWord += charOp4.charAt(Math.floor(Math.random() * charOp4.length));
+
+        // lower + upper
+      } else if (
+        alphaLow === true &&
+        alphaCap === true &&
+        numbers === false &&
+        charSpec === false
+      ) {
+        pWord += charOpA.charAt(Math.floor(Math.random() * charOpA.length));
+
+        // lower + numbers
+      } else if (
+        alphaLow &&
+        numbers &&
+        numbers === false &&
+        charSpec === false
+      ) {
+        pWord += charOpB.charAt(Math.floor(Math.random() * charOpB.length));
+
+        // lower + spec
+      } else if (
+        alphaLow === true &&
+        alphaCap === false &&
+        numbers === false &&
+        charSpec === true
+      ) {
+        pWord += charOpC.charAt(Math.floor(Math.random() * charOpC.length));
+
+        // upper + numbers
+      } else if (
+        alphaLow === false &&
+        alphaCap === true &&
+        numbers === true &&
+        charSpec === false
+      ) {
+        pWord += charOpD.charAt(Math.floor(Math.random() * charOpD.length));
+
+        // upper +spec
+      } else if (
+        alphaLow === false &&
+        alphaCap === true &&
+        numbers === false &&
+        charSpec === true
+      ) {
+        pWord += charOpE.charAt(Math.floor(Math.random() * charOpE.length));
+
+        //number + spec
+      } else if (
+        alphaLow === false &&
+        alphaCap === false &&
+        numbers === true &&
+        charSpec === true
+      ) {
+        pWord += charOpF.charAt(Math.floor(Math.random() * charOpF.length));
       }
     }
   }
-  console.log(pWord);
+  return pWord;
 }
 
 // Assignment Code

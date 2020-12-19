8aef0f7064ae3f0726e1627c9e65e0130be82e25
Jaja Brown <65190496+jbrown827@users.noreply.github.com>
2020-12-18 21:52:16 -0600

    Declare variables

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+/* Variable declaration */
+
+// Character variables
+
+var lower = "abcdefghijklmnopqrstuvwxyz";
+var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var nums = "0123456789";
+var spec = "~!@#$%/^+&*-()[]_{}<>?";
+
+// User input variables
+
+var userInput = prompt(
+  "Please choose a number from 8 to 128 for your password length"
+);
+
+var alphaLow = confirm("Would you like to use lowercase letters?");
+var alphaCap = confirm("Would you like to use uppercase letters?");
+var numbers = confirm("Would you like to you use numbers also?");
+var charSpec = confirm("How about special characters?");
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
@@ -7,7 +27,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
